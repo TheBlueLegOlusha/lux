fddbe066a9715b8ee48bffc8e001d6bf70c0531f
TheBlueLegOlusha <TheBlueLegOlusha@mail.ru>
2024-04-21 01:52:25 +0300

    1

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,80 +1,81 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 using  namespace std;
-struct village
-{
-    int region_number;
-};
-struct region
-{
-    string extensions[3]{"garage","barn","bathhouse"};
-    int house_foloor;
-};
+struct matrix{
+int matr[3][3];
+  void multi(int a[3][3]) {
+    int result[3][3]; // Временная матрица для хранения результата
 
-struct house
-{
-    string floors[3];
-};
-struct rooms
-{
-    string room[5]{"kitchen","bedroom","bath","child","guest"}; 
-};
-
-
-
-
-int main()
-{
-    village region_number;
-    region extensions;
-    region house_floor;
-    house floors;
-    rooms room;
-    int on;
-    cin >> on;
-    int i = 0;
-    while (on)
-    {
-        int f;
-        int r;
-        if(i%2==0)
-        {
-            region_number.region_number = (1000-i);
-            if(i%2==0 and i%3!=0)
-            {
-                f = 3;
-                r =0;
-            }
-            if(i%2==0 and i%3==0)
-            {
-                f=2;
-                r = 2;
-            }
+    // Вычисляем матричное произведение и сохраняем результат во временную матрицу
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        result[i][j] = 0;
+        for (int k = 0; k < 3; k++) {
+          result[i][j] += matr[i][k] * a[k][j];
         }
-        else
-        {
-            region_number.region_number = (1000+i);
-            f = 1;
-            r=1;
-        }
-        house_floor.house_foloor=r+1;
-        cout << region_number.region_number << " " << extensions.extensions[f-1] << " " << house_floor.house_foloor <<" " ;
-        for(int k = 1; k<=f; k++)
-        {
-            cout <<"floor=" << k <<" ";
-            for(int l=0 ; l<=r; l++)
-            {
-                for(int h = 0; h<5; h++){}
-                cout  << room.room[l] << " " ;
-            }
-        }
-        
-        
-        i++;
-        cin >> on;
-            
+      }
     }
-        
-    
+
+    // Копируем результат обратно в матрицу matr
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        matr[i][j] = result[i][j];
+      }
+    }
+  }
+
+
+  void random() {
+    srand(time(NULL));
+    for(int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        matr[i][j] = rand();
+      }
+    }
+  }
+void minus(int a){
+  for(int i = 0; i<3; i++){
+    for(int j = 0; j<3; j++){
+      matr[i][j]=matr[i][j]-a;
+    }
+  }
 }
-    
+void pluse(int a[3][3]){
+  for (int i = 0; i<3; i++){
+    for(int j = 0; j<3 ; j++){
+      matr[i][j]=matr[i][j]+a[i][j];
+    }
+    cout << "\n";
+  }
+}
+void print(){
+  for (int i = 0; i<3; i++){
+    for(int j = 0; j<3 ; j++){
+      cout << matr[i][j] << " ";
+    }
+    cout << "\n";
+  }
+
+}
+};
+int main() {
+  srand(time(NULL));
+  matrix mass;
+  int a[3][3];
+    for(int i = 0; i<3; i++){
+      for(int j = 0; j<3; j++){
+        cin >> mass.matr[i][j];
+      }
+    }
+  for(int i = 0; i<3; i++){
+    for(int j = 0; j<3; j++){
+      cin >> a[i][j];
+    }
+  }
+
+  //mass.pluse(a);
+ //mass.random();
+  mass.multi(a);
+  mass.print();
+}
